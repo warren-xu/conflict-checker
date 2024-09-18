4e74599c14def9b6dad71c205011e3d118d2cc0b
Warren Xu <wrxu@uwaterloo.ca>
2024-09-17 23:46:42 -0400

    Update upload.js
    Added comments for clarity

--- backend-src/src/middleware/upload.js
+++ backend-src/src/middleware/upload.js
@@ -1,7 +1,7 @@
 
 const multer = require("multer");
 
-const csvFilter = (req, file, cb) => {
+const csvFilter = (req, file, cb) => {                                    // function to only accept csv type files, can expand further 
     if(file.mimetype.includes("csv")) {
         cb(null, true);
     } else {
@@ -15,9 +15,9 @@ var storage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
         console.log(file.originalname);
-        cb(null, `${Date.now()}-Warren-${file.originalname}`);
+        cb(null, `${Date.now()}-Warren-${file.originalname}`);            // Create copies of each upload for backups
     },
 });
 
-var uploadFile = multer({ storage: storage, fileFilter: csvFilter});
+var uploadFile = multer({ storage: storage, fileFilter: csvFilter});      // using multer to read the multipart form data of a csv file
 module.exports = uploadFile;
