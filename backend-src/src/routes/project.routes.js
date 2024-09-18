dadfeb18f7cf91ef3fb6adfa9c1ebefd96b10723
Warren Xu <warrenxu2017@gmail.com>
2024-09-17 22:50:58 -0400

    Update project.routes.js
    Cleaned up comments

--- backend-src/src/routes/project.routes.js
+++ backend-src/src/routes/project.routes.js
@@ -4,12 +4,11 @@ const csvController = require("../controllers/projects/csv.controller");
 const upload = require("../middleware/upload");
 
 let routes = (app) => {
-  router.post("/upload", upload.single("csv"), csvController.upload);
-  router.get("/projects", csvController.getAddresses);
-  router.post("/geocode", csvController.geoCode);
-  // router.delete("/projects", csvController.delProjects);
+  router.post("/upload", upload.single("csv"), csvController.upload);      // routes for each REST API (upload csv)
+  router.get("/projects", csvController.getAddresses);                     // get addresses of all projects
+  router.post("/geocode", csvController.geoCode);                          // geocode addresses into lat/long
 
-  app.use("/api/csv", router);
+  app.use("/api/csv", router);              // path to referencing apis
 };
 
 module.exports = routes;  
