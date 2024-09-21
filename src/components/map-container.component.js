0ba2622918184f278289d55cc60c1966342a083f
Warren Xu <warrenxu2017@gmail.com>
2024-09-21 16:10:45 -0400

    Updated website code

--- src/components/map-container.component.js
+++ src/components/map-container.component.js
@@ -1,6 +1,6 @@
 // MapContainer.js
 import React, { useState, useEffect } from 'react';
-import { GoogleMap, LoadScript, Marker, InfoWindow} from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, InfoWindow, Autocomplete} from '@react-google-maps/api';
 import ProjectDataService from "../services/upload-files.service";
 //import axios from 'axios';
 
@@ -17,6 +17,8 @@ const center = {
 const MapContainer = () => {
   const [markers, setMarkers] = useState([]);
   const [selectedMarker, setSelectedMarker] = useState(null);
+  const [map, setMap] = useState(null);
+  const [autocomplete, setAutocomplete] = useState(null);
   
   useEffect(() => {
     const fetchAddresses = async () => {
@@ -28,7 +30,6 @@ const MapContainer = () => {
         
         const results = await ProjectDataService.geocode(addresses);  // geocode them
         setMarkers(results.data);
-        console.log('Markers:', markers);
         
       } catch (error) {
         console.error('Error fetching addresses:', error);
@@ -36,26 +37,90 @@ const MapContainer = () => {
     };
     fetchAddresses();
   }, []);   // Empty
+
+  const handlePlaceSelected = (marker) => {
+    setMarkers((current) => [...current, {...marker, fromSearchBar: true}]);  // add a fromsearchbar element to differentiate the searched marker from the db markers
+    if (map) {
+      map.panTo(marker.location); // Make sure marker.location is correct
+      map.setZoom(16); // Zoom in on the marker
+    } else {
+      console.error("Map instance is not initialized.");
+    }
+  };
+
+  const onLoad = mapInstance => {
+    setMap(mapInstance);
+  }
+
+  const onAutocompleteLoad = (autocompleteInstance) => {
+    setAutocomplete(autocompleteInstance);
+  };
+
+  const onPlaceChanged = () => {
+    if (autocomplete !== null) {
+      const place = autocomplete.getPlace();
+      const location = {
+        lat: place.geometry.location.lat(),
+        lng: place.geometry.location.lng(),
+      };
+
+      const newMarker = {
+        location,
+        address: place.formatted_address,
+      };
+
+      handlePlaceSelected(newMarker);
+    } else {
+      console.error("Autocomplete is not loaded yet!");
+    }
+  };
   
   return (
 
     <div>
       <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY} libraries={['places']}>
+      <Autocomplete onLoad={onAutocompleteLoad} onPlaceChanged={onPlaceChanged}>
+          <input
+            type="text"
+            placeholder="Search for a location"
+            style={{
+              boxSizing: `border-box`,
+              border: `1px solid transparent`,
+              width: `240px`,
+              height: `32px`,
+              padding: `0 12px`,
+              borderRadius: `3px`,
+              boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+              fontSize: `14px`,
+              outline: `none`,
+              textOverflow: `ellipses`,
+              position: "absolute",
+              left: "50%",
+              marginLeft: "-120px",
+              top: "10px",
+            }}
+          />
+        </Autocomplete>
         
-        
-        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10} onLoad={onLoad}>
           
-          {markers.length > 0 ? markers.map((marker, index) => (
+          {/* {markers.length > 0 ? markers.map((marker, index) => (
             <Marker
               key={index}
               position={marker.location}
               onClick={() => setSelectedMarker(marker)}
             />
-          )) : markers.map((marker, index) => (
+          )) :  */}{markers.map((marker, index) => (
             <Marker
               key={index}
               position={marker.location}
               onClick={() => setSelectedMarker(marker)}
+              icon={{
+                url: marker.fromSearchBar
+                 ? "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+                 : undefined // Blue for searched marker
+                  
+              }}
             />
           ))}
           {selectedMarker && (
